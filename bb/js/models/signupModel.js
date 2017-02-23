39ce90d37ca2834f3e1d500bb2eb3378e8112924
Raju Samala <rsamala.osicus@gmail.com>
2017-02-23 16:32:09 +0530

    temp commit

--- bb/js/models/signupModel.js
+++ bb/js/models/signupModel.js
@@ -1,7 +1,8 @@
 define([
     'backbone',
-    'handlebars'
-], function (Backbone, handlebars) {
+    'handlebars',
+    'underscore'
+], function (Backbone, handlebars,_) {
     'use strict';
     var signupModel = Backbone.Model.extend({
         defaults: {
@@ -11,22 +12,61 @@ define([
             // passwd: 'password',
             icode: 'nocode'
         },
-        validate : function(atts){
-            if(!attrs.email){
-                return 'Please fill email field'
-            }
-            if(!attrs.firstname){
-                return 'Please fill firstname field'
-            }
-            if(!attrs.lastname){
-                return 'Please fill lastname field'
-            }
-            if(!attrs.passwd){
-                return 'Please fill Password field'
-            }
-            if(attrs.passwd || atts.re_pwd){
-                return 'Re-enter passwords please, they dont match '
-            }
+        initialize: function () {
+            this.on('invalid', function (model, errors, options) {
+               console.log(model.validationError);
+               console.log(model.attributes.email.isValid);
+               console.log(options.validationError);
+            });
+        },
+        validate: function (attrs, options) {
+            console.log(attrs);
+            console.log(options);
+
+            var error={};
+            console.log(_.size(attrs));
+           var keys=_.keys(attrs);
+           console.log(keys);
+           for(var i=1;i<keys.length;i++){
+               if(keys[i]==""){
+                   return 'please enter'+keys[i]
+               }
+           }
+          
+            // if(!attrs.email){
+            //     error.attrs.email='Please fill email field';
+            // }
+            // if(!attrs.firstname){
+            //     error.attrs.firstname='Please fill firstname field';
+            // }
+            // if(!attrs.lastname){
+            //    error.attrs.lastname='Please fill lastname field';
+            // }
+            // if(!attrs.passwd){
+            //     error.apush('Please fill Password field');
+            // }
+            // if(attrs.passwd || attrs.re_pwd){
+            //     error.push('Re-enter passwords please, they dont match ');
+            // }
+            // return error;
+
+            // var error = [];
+            // if (!attrs.email) {
+            //     $('.email').text('Please fill email field');
+            // }
+            // if (!attrs.firstname) {
+            //     return ('Please fill firstname field');
+            // }
+            // if (!attrs.lastname) {
+            //     return ('Please fill lastname field');
+            // }
+            // if (!attrs.passwd) {
+            //     return ('Please fill Password field');
+            // }
+            // if (attrs.passwd || attrs.re_pwd) {
+            //     return ('Re-enter passwords please, they dont match ');
+            // }
+            // return error;
         }
     });
     return signupModel;
