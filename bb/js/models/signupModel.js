caa0a21581239bda1e085b235045676dfe2977e3
Raju Samala <rsamala.osicus@gmail.com>
2017-02-25 14:11:05 +0530

    field -level validations implemented successfully

--- bb/js/models/signupModel.js
+++ bb/js/models/signupModel.js
@@ -5,59 +5,30 @@ define([
     'use strict';
     var signupModel = Backbone.Model.extend({
         defaults: {
-            // email: 'test@test.com',
-            // firstname: 'firstName',
-            // lastname: 'lastName',
-            // passwd: 'password',
+
         },
         initialize: function () {
-            this.on('invalid', function (model, errors, options) {
-                // console.log(model.validationError);
-                // console.log(errors);
-                // console.log(options.validationError);
-            });
+
         },
         validate: function (attrs, options) {
-            // console.log(attrs);
-            // console.log(options);
-            // var keys = _.keys(attrs);
-            // console.log(keys);
+            var error_msg = {};
 
-            var error = [];
             if (!attrs.email) {
-                error.push('Please fill email field');
+                error_msg.email = ('Please fill email field');
             }
             if (!attrs.firstname) {
-                error.push('Please fill firstname field');
+                error_msg.firstname = ('Please fill firstname field');
             }
             if (!attrs.lastname) {
-                error.push('Please fill lastname field');
+                error_msg.lastname = ('Please fill lastname field');
             }
             if (!attrs.passwd) {
-                error.push('Please fill Password field');
+                error_msg.passwd = ('Please fill Password field');
             }
-            if (attrs.passwd !== attrs.re_pwd) {
-                error.push('Re-enter passwords please, they dont match ');
+            if (attrs.passwd || attrs.re_pwd) {
+                error_msg.re_pwd = ('Re-enter passwords please, they dont match ');
             }
-            return error;
-
-            // var error = [];
-            // if (!attrs.email) {
-            //     $('.email').text('Please fill email field');
-            // }
-            // if (!attrs.firstname) {
-            //     return ('Please fill firstname field');
-            // }
-            // if (!attrs.lastname) {
-            //     return ('Please fill lastname field');
-            // }
-            // if (!attrs.passwd) {
-            //     return ('Please fill Password field');
-            // }
-            // if (attrs.passwd || attrs.re_pwd) {
-            //     return ('Re-enter passwords please, they dont match ');
-            // }
-            // return error;
+            return error_msg;
         }
     });
     return signupModel;
