21b236e8ccc6d76a954913edd4550f487ed51f20
Raju Samala <rsamala.osicus@gmail.com>
2017-02-25 16:10:45 +0530

    Email regex validation added

--- bb/js/models/signupModel.js
+++ bb/js/models/signupModel.js
@@ -12,9 +12,14 @@ define([
         },
         validate: function (attrs, options) {
             var error_msg = {};
+            var keys = {
+                email :  /^([-0-9a-zA-Z'_\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            };
 
             if (!attrs.email) {
                 error_msg.email = ('Please fill email field');
+            }else if (!keys.email.test(attrs.email)){
+                error_msg.email = ('Please enter valid email');
             }
             if (!attrs.firstname) {
                 error_msg.firstname = ('Please fill firstname field');
